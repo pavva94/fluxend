8626f825e62d03c81510290f55fa853e940b3ece
Alessandro Pavesi <pavesialessandro@live.it>
2016-06-16 23:50:32 +0200

    Creazione griglia in Start, movimento griglia verso il basso automaticamente e destra e sinistra controllato dal giocatore.

---
+++ Assets/Scripts/CreateMap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMap : MonoBehaviour {
+
+    public GameObject prefab;
+    public float gridX = 5f;
+    public float gridY = 5f;
+    public float spacing = 2f;
+
+    void Start()
+    {
+        for (int y = 0; y < gridY; y++)
+        {
+            for (int x = 0; x < gridX; x++)
+            {
+                Vector3 pos = new Vector3(x, y, 0) * spacing;
+                Instantiate(prefab, pos, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
---
+++ Assets/Scripts/MoveObstacles.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObstacles : MonoBehaviour {
+
+    public float speed;
+    private float minX, maxX, minY, maxY;
+
+    new Rigidbody2D rigidbody;
+
+    // Use this for initialization
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
+        Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
+
+        minX = bottomCorner.x;
+        maxX = topCorner.x;
+        minY = bottomCorner.y;
+        maxY = topCorner.y;
+    }
+
+    // Update is called once per frame
+    void Update() { 
+        // movimento utente blocchi
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        // movimento verticale costante blocchi
+        float moveVertical = -2.0f;
+        
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rigidbody.velocity = movement * speed;
+        //Debug.Log(transform.position.x);
+        if (moveHorizontal > 0) transform.Translate(Vector3.right * Time.deltaTime);
+        else if (moveHorizontal < 0) transform.Translate(Vector3.left * Time.deltaTime);
+
+    }
+}
