644b196f82a6384daa2efce23996c587e771c955
Alessandro Pavesi <pavesialessandro@live.it>
2016-07-06 23:56:15 +0200

    Inseriti i menu di gioco, assests StandardAssets

---
+++ Assets/Scripts/MenuButtonLoadLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonLoadLevel : MonoBehaviour {
+
+	public void loadLevel(string leveltoLoad)
+	{
+		Application.LoadLevel (leveltoLoad);
+	}
+}
