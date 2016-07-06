d3ff4faa60a581fad4bdd49b0ec81c6d5f1d1ba1
Alessandro Pavesi <pavesialessandro@live.it>
2016-07-06 23:07:47 +0200

    Inserito touch

--- Assets/Scripts/MoveObstacles.cs
+++ Assets/Scripts/MoveObstacles.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class MoveObstacles : MonoBehaviour {
 
     public float speed;
-    private float minX, maxX, minY, maxY;
+    private float screenCenterX;
+    private float moveVertical;
 
     new Rigidbody2D rigidbody;
 
@@ -12,30 +13,48 @@ public class MoveObstacles : MonoBehaviour {
     void Start() {
         rigidbody = GetComponent<Rigidbody2D>();
 
-        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
-        Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
-        Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
-
-        minX = bottomCorner.x;
-        maxX = topCorner.x;
-        minY = bottomCorner.y;
-        maxY = topCorner.y;
+        screenCenterX = Screen.width * 0.5f;
+        moveVertical = 0.2f;
     }
 
     // Update is called once per frame
-    void Update() { 
+    void Update() {
         // movimento utente blocchi
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        // movimento verticale costante blocchi
-        float moveVertical = -0.9f;
-        
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rigidbody.velocity = movement * speed;
-        //Debug.Log(transform.position.x);
-        if (moveHorizontal > 0) transform.Translate(Vector3.right * Time.deltaTime);
-        else if (moveHorizontal < 0) transform.Translate(Vector3.left * Time.deltaTime);
+        transform.Translate(Vector3.down * moveVertical);
+        // if there are any touches currently
+        if (Input.touchCount > 0)
+        {
+            // get the first one
+            Touch firstTouch = Input.GetTouch(0);
 
-    }
+            // if it began this frame
+            if (firstTouch.phase == TouchPhase.Moved || firstTouch.phase == TouchPhase.Stationary)
+            {
+                if (firstTouch.position.x > screenCenterX)
+                {
+                    // if the touch position is to the right of center
+                    // move right
+                    transform.Translate(speed, Vector3.down.y * moveVertical, 0);
+                }
+                else if (firstTouch.position.x < screenCenterX)
+                {
+                    // if the touch position is to the left of center
+                    // move left
+                    transform.Translate(-speed, Vector3.down.y * moveVertical, 0);
+                }
+            }
+        }
+    
+    // movimento verticale costante blocchi
+    /*float moveVertical = -0.1f;
+
+    Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+    rigidbody.velocity = movement * speed;
+    //Debug.Log(transform.position.x);
+    if (moveHorizontal > 0) transform.Translate(Vector3.right * Time.deltaTime);
+    else if (moveHorizontal < 0) transform.Translate(Vector3.left * Time.deltaTime);*/
+
+}
 
     void OnCollisionEnter2D(Collision2D coll)
     {
