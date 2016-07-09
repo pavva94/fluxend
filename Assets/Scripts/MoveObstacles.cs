077a0b89dbfdad18988f8690d8dfa179d2cf5396
Alessandro Pavesi <pavesialessandro@live.it>
2016-07-09 13:15:04 +0200

    Bugfix menu, implemementata un minimo di GUI non troppo funzionante, bugfix collisioni.

--- Assets/Scripts/MoveObstacles.cs
+++ Assets/Scripts/MoveObstacles.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
 
 public class MoveObstacles : MonoBehaviour {
 
@@ -7,18 +11,32 @@ public class MoveObstacles : MonoBehaviour {
     private float screenCenterX;
     public float moveVertical;
 
+    // game performance
+    public int score = 0;
+    public int startEnergy = 50;
+    public int energy;
+
+    // UI elements to control
+    public Text UIScore;
+    public Text UIEnergy;
+    public GameObject UIGameOver;
+
     new Rigidbody2D rigidbody;
 
     // Use this for initialization
     void Start() {
+        UIGameOver.SetActive(false); // disattiva il text gameOver
+
         rigidbody = GetComponent<Rigidbody2D>();
 
         screenCenterX = Screen.width * 0.5f;
-        // moveVertical = 0.2f;
+
+        energy = startEnergy;
+        refreshGUI();
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
         // movimento utente blocchi
         transform.Translate(Vector3.down * moveVertical);
         // if there are any touches currently
@@ -44,23 +62,68 @@ public class MoveObstacles : MonoBehaviour {
                 }
             }
         }
-    
-    // movimento verticale costante blocchi
-    /*float moveVertical = -0.1f;
-
-    Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-    rigidbody.velocity = movement * speed;
-    //Debug.Log(transform.position.x);
-    if (moveHorizontal > 0) transform.Translate(Vector3.right * Time.deltaTime);
-    else if (moveHorizontal < 0) transform.Translate(Vector3.left * Time.deltaTime);*/
-
-}
+        AddPoints((int) Time.timeSinceLevelLoad);
+    }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
         Debug.Log(coll.gameObject.tag);
         if (coll.gameObject.tag == "Player")
-            Time.timeScale = 0;
+        {
+            // rigidbody.velocity = Vector3.zero;
+            // Time.timeScale = 0;
+            // carico la prossima scena
+            // SceneManager.LoadScene("OneMoreChance");
+            RemoveEnergy(10);
+            if (energy <= 0)
+            {
+                Debug.Log("GAME OVER");
+                UIGameOver.SetActive(true); // this brings up the gameOver UI
+                Time.timeScale = 0f;
+                // SceneManager.LoadScene("OneMoreChance");
+            }
+        }
+    }
 
+    // refresh all the GUI elements
+    void refreshGUI()
+    {
+        // set the text elements of the UI
+        UIScore.text = "Score: " + score.ToString();
+        UIEnergy.text = "Energy: " + energy.ToString();
+    }
+
+    // public function to add points and update the gui and highscore player prefs accordingly
+    public void AddPoints(int amount)
+    {
+        // increase score
+        score += amount;
+
+        // update UI
+        UIScore.text = "Score: " + score.ToString();
+
+        // if score>highscore then update the highscore UI too
+        /*if (score > highscore)
+        {
+            highscore = score;
+            UIHighScore.text = "Highscore: " + score.ToString();
+        }*/
+    }
+
+    // public function to add points and update the gui and highscore player prefs accordingly
+    public void RemoveEnergy(int amount)
+    {
+        // increase score
+        energy -= amount;
+
+        // update UI
+        UIEnergy.text = "Energy: " + score.ToString();
+
+        // if score>highscore then update the highscore UI too
+        /*if (score > highscore)
+        {
+            highscore = score;
+            UIHighScore.text = "Highscore: " + score.ToString();
+        }*/
     }
 }
