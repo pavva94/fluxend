691ef30e0c7c2844c4044e51e8f365fe1ae7de3a
Alessandro Pavesi <pavesialessandro@live.it>
2016-09-13 21:25:08 +0200

    bugfix magnete, ora funziona correttamente

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     private float startTime;
     private float journeyLength;
     private Vector2 bottomCorner;
+    private bool in_Collision = false;
 
     void Start()
 	{
@@ -35,6 +36,7 @@ public class PlayerController : MonoBehaviour
         minY = bottomCorner.y;
         maxY = topCorner.y;
         */
+        rigidbody = GetComponent<Rigidbody2D>();
         startPosition = new Vector3(startPoint.transform.position.x, startPoint.transform.position.y, 0.0f);
         transform.position = startPosition;
         startTime = Time.time;
@@ -47,14 +49,29 @@ public class PlayerController : MonoBehaviour
     {
         //Debug.Log(transform.position.x);
         //Debug.Log(transform.position.y);
-        float distCovered = (Time.time - startTime) * speed;
-        float fracJourney = distCovered / journeyLength;
-        transform.position = Vector2.Lerp(transform.position, startPoint.transform.position, fracJourney);
+        if (!in_Collision)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector2.Lerp(transform.position, startPoint.transform.position, fracJourney);
+        }
+        //Vector2 direction = transform.position - startPoint.transform.position;
+        //rigidbody.AddForceAtPosition(direction.normalized, transform.position);
 
         if (transform.position.y < bottomCorner.y - 10)
         {
             GameManager.gameOver();
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        in_Collision = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        in_Collision = false;
     }
 
     /*void OnBecameInvisible() {
